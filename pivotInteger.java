319dbdd89e3335f53828c14301876434f421bd6b
Aditya Jurel <148925887+Adityajurel@users.noreply.github.com>
2026-03-29 11:43:21 +0530

    Create pivotInteger.java

---
+++ pivotInteger.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int total = n*(n+1)/2;
+        for(int i =1;i<=n;i++){
+            int prev = i*(i+1)/2;
+            int next=i+(total-prev);
+            if(prev == next) return i;   
+        }
+     return -1;
+    }
+}
