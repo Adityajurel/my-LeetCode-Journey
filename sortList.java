51124e98df0b35c8f1ad99ef18abf4dbe3c3967b
Aditya Jurel <148925887+Adityajurel@users.noreply.github.com>
2026-04-07 12:34:38 +0530

    Create sortList.java

---
+++ sortList.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+        ListNode d = new ListNode(-1);
+        ListNode ans = d;
+        while(t1!=null&&t2!=null){
+            if(t1.val<=t2.val){
+                ans.next =t1;
+                ans =t1;
+                t1=t1.next;
+            }
+            else{
+                ans.next =t2;
+                ans = t2;
+                t2=t2.next;
+            }
+        }
+        if(t1==null){
+            ans.next =t2;
+            ans = t2;
+        }
+        else{
+             ans.next =t1;
+             ans = t1;
+        }
+        return d.next;
+    }
+
+
+
+    public ListNode sortList(ListNode head) {
+         if(head == null||head.next == null) return head;
+        ListNode s= head;
+        ListNode f = head;
+        while(f.next!=null&&f.next.next!=null){
+            s= s.next;
+            f =f.next.next;
+        }
+        ListNode new2=s.next;
+        s.next = null;
+       head= sortList(head);
+        new2=sortList(new2);
+        return mergeTwoLists(head,new2);
+
+    }
+}
