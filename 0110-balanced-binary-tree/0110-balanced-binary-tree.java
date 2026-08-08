b18ca296098cf3526f49732101ec3da4319371e5
Aditya Jurel <148925887+Adityajurel@users.noreply.github.com>
2026-08-08 10:53:02 +0530

    Time: 0 ms (100%), Space: 45.4 MB (77.76%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-    boolean flag;
+   boolean flag=true;
     int ib(TreeNode root){
         if(root==null) return 0;
-
-        int lh=ib(root.left);
-        int rh=ib(root.right);
-        if(Math.abs(lh-rh)>1)flag=false;
+        
+        int lh= ib(root.left);
+        int rh = ib(root.right);
+        if(Math.abs(lh-rh)>1) flag= false;
         return 1+Math.max(lh,rh);
     }
     public boolean isBalanced(TreeNode root) {
-         flag= true;
         ib(root);
         return flag;
     }
