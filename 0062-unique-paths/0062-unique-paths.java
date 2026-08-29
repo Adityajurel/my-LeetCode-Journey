239d3f00a9e8b8344d4ee3d837ee73f4fb2da419
Aditya Jurel <148925887+Adityajurel@users.noreply.github.com>
2026-08-29 11:10:45 +0530

    Time: 0 ms (100%), Space: 42.2 MB (47.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,7 @@
 class Solution {
-    int [][] dp ;
+    
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
+     int [][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0||j==0)dp[i][j]=1;
